b181d7e25049591421ac6815923eb7e7e36150af
MarTCM <marwane.elbaraka@gmail.com>
2021-01-03 15:14:31 +0100

    fixed api port issues

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: "http://localhost:" + process.env.PORT + "/api",
 });
 
 export const insertArticle = (payload) => api.post(`/article`, payload);
