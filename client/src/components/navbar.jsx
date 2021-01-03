391e73d50f01e043f0723a87ffe6e0f7cdbcd81a
MarTCM <marwane.elbaraka@gmail.com>
2021-01-03 19:04:36 +0100

    added icon and modified navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -11,35 +11,25 @@ class Navbar extends Component {
           <Link className="navbar-brand navbar-header Roboto-Bold" to="#">
             Revue Ecologique
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <Link
-                className="nav-item nav-link m-"
+                className="nav-item nav-link"
                 to="/"
                 style={{ fontSize: 20 }}
               >
                 Acceuil
               </Link>
               <Link
-                className="nav-item nav-link m-"
+                className="nav-item nav-link"
                 to="/articles"
                 style={{ fontSize: 20 }}
               >
                 Articles
               </Link>
               <Link
-                className="nav-item nav-link m-"
+                className="nav-item nav-link"
                 to="/admin"
                 style={{ fontSize: 20 }}
               >
