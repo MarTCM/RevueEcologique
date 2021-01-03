61a669253af1f1e4e5049030e8c671030b831d6e
MarTCM <marwane.elbaraka@gmail.com>
2021-01-03 14:55:40 +0100

    6

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:" + process.env.PORT + "/api",
+  baseURL: "http://localhost:8001/api",
 });
 
 export const insertArticle = (payload) => api.post(`/article`, payload);
