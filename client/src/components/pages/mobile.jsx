e605150499065e3cbeb2a0e6ee22bfe3f1803992
MarTCM <marwane.elbaraka@gmail.com>
2021-01-11 20:44:13 +0100

    updated mobile app download link

--- client/src/components/pages/mobile.jsx
+++ client/src/components/pages/mobile.jsx
@@ -11,7 +11,7 @@ class Mobile extends Component {
           Vous utilisez un appareil mobile? Télécharger l'application Revue
           Ecologique pour android.
         </div>
-        <form action="https://download1481.mediafire.com/4hrym81ry14g/b2qmp16eobwv72b/RevueEcologiqueMobile.apk">
+        <form action="https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40martcm/RevueEcologiqueMobile-90387ab0d8e14b6c95c9b940e437010b-signed.apk">
           <button className="btn btn-success" type="submit">
             Télécharger
           </button>
