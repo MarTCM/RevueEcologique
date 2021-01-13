53d3996389221d63cd5ef0b03af7d0316d84635b
MarTCM <marwane.elbaraka@gmail.com>
2021-01-13 13:56:57 +0100

    updated server.js

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
